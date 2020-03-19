b4d6c50d541a570252168eab6d2b812e92b2460e
Leo Vigna <leo@joinsaturn.com>
2020-03-19 02:07:22 +0100

    Updates

---
+++ precache-manifest.7d69113ab74bcae8f9032e51258f16e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce64b34f5bb35b8de01b8b2039b4732e",
+    "url": "/react-starter-ts/index.html"
+  },
+  {
+    "revision": "eb2da411932ed3885804",
+    "url": "/react-starter-ts/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5f23939cf5a39cbc4ffc",
+    "url": "/react-starter-ts/static/js/2.cd7f93d6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-starter-ts/static/js/2.cd7f93d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb2da411932ed3885804",
+    "url": "/react-starter-ts/static/js/main.6582146f.chunk.js"
+  },
+  {
+    "revision": "c5b1d72c30aec6afe394",
+    "url": "/react-starter-ts/static/js/runtime-main.e2781517.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-starter-ts/static/media/logo.5d5d9eef.svg"
+  }
+]);
