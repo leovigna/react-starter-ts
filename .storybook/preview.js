257b03e85f2e2766a9f7cf02149de572e2ec5ec8
Leo Vigna <leo.vigna@gmail.com>
2021-12-13 12:00:30 +0100

    update starter template

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,34 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/styles/index.css';
+import '../src/styles/animations.tsx';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: {
+        argTypesRegex: "^on[A-Z].*"
     },
-  },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+    backgrounds: {
+        default: 'main',
+        values: [
+            {
+                name: 'main',
+                value: '#252525',
+            },
+        ]
+    }
 }
+
+export const decorators = [
+    (Story) => (
+        <Router>
+            <Story />
+        </Router>
+    ),
+];
